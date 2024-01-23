88aa276625603d7ab61112562eef4c6984f6f79a
rayhanbiju <rayhanbiju@vt.edu>
2024-01-23 18:06:26 -0500

    edit to fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,11 @@
 def fizzbuzz():
-	for i in range(1, 20):
+	for i in range(1, 21):
 		if i % 3 == 0 and i % 5 == 0: 
 			print("FizzBuzz")
 		elif i % 3 == 0:
 			print("Fizz")
 		elif i % 5 == 0: 
-			print("Buzz)
+			print("Buzz")
 		else:
 			print(i) 
-
 
